316420c451ec5db7d804b7d90a4e267f5b97f3c0
NanduReddy9849 <enandivardhan2002@gmail.com>
2025-04-02 23:42:54 +0530

    first commit

---
+++ Details.py
@@ -0,0 +1,25 @@
+""" My Details """
+
+name=input("Enter a Name:")
+age=int(input("Enter the age:"))
+address=input("Enter the address:")
+print()
+print("Enter a Name:",name)
+print("Enter The Age:",age)
+print("Enter the address:",address)
+
+""" Arithmetic Operations """
+
+n1=int(input("Enter a Number1:"))
+n2=int(input("Enter a Number2:"))
+print(n1+n2)
+print(n1-n2)
+print(n1*n2)
+print(n1/n2)
+print(n1//n2)
+print(n1%n2)
+
+""" Type """
+
+sum=30
+print(type(sum))
